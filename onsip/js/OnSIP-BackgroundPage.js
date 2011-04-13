06ad596d0ee67d63acb69ccc4105546ffa77b248
OnSIP Developer <developer@junctionnetworks.com>
2011-04-13 09:09:43 -0400

    Reformatting code

--- onsip/js/OnSIP-BackgroundPage.js
+++ onsip/js/OnSIP-BackgroundPage.js
@@ -25,19 +25,20 @@ var name_from_context = '';
 OX_EXT.apps = [BG_APP];
 
 if (pref && pref.get('onsipCredentialsGood') === true && pref.get('onsipPassword') && pref.get('fromAddress')) {
-    if (pref.get('onsipPassword').length > 0 && pref.get('fromAddress').length > 0) {
-        OX_EXT.init (pref, {
-            onSuccess : function () {
-		dbg.log(BG_LOG, 'Succeeded in OX_EXT.init for connecting & subscribing');
-            },
-            onError   : function (error) {
-                /** In case of failure, display settings in a new tab **/
-		dbg.log(BG_LOG, 'There was an error in OX_EXT.init ' + error);
-            }
-	});
-    } else {
-	dbg.log(BG_LOG, 'OX_EXT.init NOT called, no credentials found');
-    }
+  if (pref.get('onsipPassword').length > 0 && pref.get('fromAddress').length > 0) {
+    OX_EXT.init (
+      pref, {
+        onSuccess : function () {
+		      dbg.log(BG_LOG, 'Succeeded in OX_EXT.init for connecting & subscribing');
+        },
+        onError   : function (error) {
+          /** In case of failure, display settings in a new tab **/
+		      dbg.log(BG_LOG, 'There was an error in OX_EXT.init ' + error);
+        }
+	    });
+  } else {
+	  dbg.log(BG_LOG, 'OX_EXT.init NOT called, no credentials found');
+  }
 };
 
 /** An extension to this background page with helper methods **/
@@ -47,17 +48,18 @@ extension.init();
 /** Load and initialize Highrise with contacts **/
 dbg.log (BG_LOG, 'Highrise Enabled --> ' + pref.get ('highriseEnabled'));
 if (pref && pref.get('highriseEnabled') === true) {
-    highrise_app.init(pref);
+  highrise_app.init(pref);
 }
 
 /** Initialize Zendesk with Contacts **/
 dbg.log (BG_LOG, 'Zendesk Enabled --> '  + pref.get ('zendeskEnabled'));
 if (pref && pref.get('zendeskEnabled') === true) {
-    zendesk_app.init(pref);
+  zendesk_app.init(pref);
 }
 
 /** Add event listener for clicks on the extension icon **/
-chrome.browserAction.onClicked.addListener ( function (TAB) {
+chrome.browserAction.onClicked.addListener (
+  function (TAB) {
     dbg.log (BG_LOG, 'clicked enable / disable icon');
     extension.toggle();
 });
@@ -68,170 +70,177 @@ var sc_interval;
 /** to re-authorize & re-subscribe to pubsub after failures in BOSH **/
 /** These failures occur as a result of putting a computer to sleep, or restarting WIFI **/
 var sc = function() {
-    if (!(pref && pref.get('onsipCredentialsGood'))){
-	dbg.log (BG_LOG, 'In -sc-, onsip credentials are no good, not running connectivity checker');
-	return;
-    }
-    if (!found_errors) {
-	found_errors = OX_EXT.strophe_conn.errors > 0;
-    }
-    if (found_errors) {
-	dbg.log (BG_LOG, 'Discovered ' + OX_EXT.strophe_conn.errors  + ' errors lets RE-ESTABLISH connection');
-	var do_exec = function() {
+  if (!(pref && pref.get('onsipCredentialsGood'))){
+	  dbg.log (BG_LOG, 'In -sc-, onsip credentials are no good, not running connectivity checker');
+	  return;
+  }
+  if (!found_errors) {
+	  found_errors = OX_EXT.strophe_conn.errors > 0;
+  }
+  if (found_errors) {
+	  dbg.log (BG_LOG, 'Discovered ' + OX_EXT.strophe_conn.errors  + ' errors lets RE-ESTABLISH connection');
+	  var do_exec = function() {
 	    OX_EXT.failures = 0;
 	    BG_APP.launched_n = false;
-	    OX_EXT.init(pref, {
-		onSuccess : function() {
-		    dbg.log (BG_LOG, 'Succeeded in OX_EXT.init for REBOUND connecting & subscribing');
-		    found_errors = false;
-		},
-		onError   : function(error) {
-		    dbg.log (BG_LOG, 'There was an error in do_exec() ' + error);
-		    found_errors = true;
+	    OX_EXT.init(
+        pref, {
+		      onSuccess : function() {
+		        dbg.log (BG_LOG, 'Succeeded in OX_EXT.init for REBOUND connecting & subscribing');
+		        found_errors = false;
+		      },
+		      onError   : function(error) {
+		        dbg.log (BG_LOG, 'There was an error in do_exec() ' + error);
+		        found_errors = true;
 	        }
 	    });
 
 	    /** Load and initialize Highrise with contacts **/
 	    if (pref && pref.get('highriseEnabled')) {
-		highrise_app.init(pref);
+		    highrise_app.init(pref);
 	    }
 
 	    /** Initialize Zendesk with Contacts **/
 	    if (pref && pref.get('zendeskEnabled')) {
-		zendesk_app.init(pref);
+		    zendesk_app.init(pref);
 	    }
 	};
-	OX_EXT.iConnectCheck(pref, {
-	    onSuccess : function() {
-		dbg.log(BG_LOG, 'Successfully connected to BOSH Server, do_exec()');
-		do_exec();
-	    },
-            onError   : function() {
-		dbg.log(BG_LOG, 'Failed to connect to BOSH Server ');
-	    }
-	});
-    }
+	  OX_EXT.iConnectCheck(
+      pref, {
+	      onSuccess : function() {
+		      dbg.log(BG_LOG, 'Successfully connected to BOSH Server, do_exec()');
+		      do_exec();
+	      },
+        onError   : function() {
+		      dbg.log(BG_LOG, 'Failed to connect to BOSH Server ');
+	      }
+	    });
+  }
 };
 
 if (pref && pref.get('onsipCredentialsGood')) {
-    sc_interval = setInterval(sc, 30000);
+  sc_interval = setInterval(sc, 30000);
 }
 
 
 /** Add listener for requests from the pages **/
-chrome.extension.onRequest.addListener( function(request, sender, sendResponse) {
+chrome.extension.onRequest.addListener(
+  function(request, sender, sendResponse) {
     dbg.log (BG_LOG, 'Request ');
 
     /** On load parse request **/
     if ( request.pageLoad && pref.get('enabled') ) {
-	dbg.log (BG_LOG, 'Send parseDOM request to Content Page from BG page');
-        sendResponse ({ parseDOM : true, fromAddress : pref.get('fromAddress')});
+	    dbg.log (BG_LOG, 'Send parseDOM request to Content Page from BG page');
+      sendResponse ({ parseDOM : true, fromAddress : pref.get('fromAddress')});
     }
 
     /** Open settings page request **/
     if ( request.openSettingsPage ) {
-        chrome.tabs.create ({ "url" : "index.html" });
+      chrome.tabs.create ({ "url" : "index.html" });
     }
 
     /** Clear Highrise client cache **/
     if ( request.clearCache ) {
-	dbg.log (BG_LOG, 'Clearing Highrise client cache');
-        highrise_app.clearCache();
+	    dbg.log (BG_LOG, 'Clearing Highrise client cache');
+      highrise_app.clearCache();
     }
 
     /** Make a Call on request **/
     if ( request.setupCall && pref.get ('enabled') ) {
-	var from_address  = pref.get('fromAddress');
-        var to_address    = request.phone_no;
-	var call_setup_id = new Date().getTime() + '' + Math.floor(Math.random() * 1000);
-	/** Name from context would ascertain the individual **/
-	/** we are calling further down the call initiation process **/
-	/** by scraping the page from which the click-to-call number was clicked **/
-	name_from_context = request.name_from_context;
-	// var clean_no     = formatPhoneNum (to_address);
-	dbg.log (BG_LOG, 'Call requested FROM: ' + from_address + ' - TO: ' + to_address);
-	OX_EXT.createCall (from_address, to_address, call_setup_id);
+	    var from_address  = pref.get('fromAddress');
+      var to_address    = request.phone_no;
+	    var call_setup_id = new Date().getTime() + '' + Math.floor(Math.random() * 1000);
+	    /** Name from context would ascertain the individual **/
+	    /** we are calling further down the call initiation process **/
+	    /** by scraping the page from which the click-to-call number was clicked **/
+	    name_from_context = request.name_from_context;
+	    // var clean_no     = formatPhoneNum (to_address);
+	    dbg.log (BG_LOG, 'Call requested FROM: ' + from_address + ' - TO: ' + to_address);
+	    OX_EXT.createCall (from_address, to_address, call_setup_id);
     }
 
     /** Verify SIP User **/
     if ( request.verifyOnSipUser ) {
-	dbg.log (BG_LOG, 'Request verify on sip user  ' +
-		 request.username + ', ***  -- ' +
-		 pref.get ('onsipHttpBase'));
+	    dbg.log (BG_LOG, 'Request verify on sip user  ' +
+		           request.username + ', ***  -- ' +
+		           pref.get ('onsipHttpBase'));
 
-	pref.set('fromAddress'  , request.username);
-	pref.set('onsipPassword', request.password);
+	    pref.set('fromAddress'  , request.username);
+	    pref.set('onsipPassword', request.password);
 
-	OX_EXT.init (pref, {
-	    onSuccess : function() {
-		dbg.log (BG_LOG, "SIP user Verified Successfully");
-		sendResponse({ok : true});
-	    },
-	    onError   : function(error) {
-		dbg.log (BG_LOG, "Error in verifying SIP User [ " + error + " ]");
-		sendResponse({ok : false});
-	    }
-	});
+	    OX_EXT.init (
+        pref, {
+	        onSuccess : function() {
+		        dbg.log (BG_LOG, "SIP user Verified Successfully");
+		        sendResponse({ok : true});
+	        },
+	        onError   : function(error) {
+		        dbg.log (BG_LOG, "Error in verifying SIP User [ " + error + " ]");
+		        sendResponse({ok : false});
+	        }
+	      });
     }
 
     /** Execute loop to verify XMPP / BOSH connection **/
     if ( request.checkConnection ) {
-	dbg.log (BG_LOG, "checkConnection - " + request.run);
-	if (sc_interval) {
-	    dbg.log (BG_LOG, "checkConnection - clear existing interval " + sc_interval);
-	    clearInterval(sc_interval);
-	}
-	if (request.run) {
-	    sc_interval = setInterval(sc, 30000);
-	}
+	    dbg.log (BG_LOG, "checkConnection - " + request.run);
+	    if (sc_interval) {
+	      dbg.log (BG_LOG, "checkConnection - clear existing interval " + sc_interval);
+	      clearInterval(sc_interval);
+	    }
+	    if (request.run) {
+	      sc_interval = setInterval(sc, 30000);
+	    }
     }
 
     /** Verify Zendesk User **/
     if ( request.verifyZendesk ) {
-       dbg.log (BG_LOG, 'Verifying Zendesk account with ' +
-		    request.zendesk_url + ' - ' + request.zendesk_usr + ' - ' + request.zendesk_pwd);
-       zendesk_app.verify ({
+      dbg.log (BG_LOG, 'Verifying Zendesk account with ' +
+		           request.zendesk_url + ' - ' + request.zendesk_usr + ' - ' + request.zendesk_pwd);
+      zendesk_app.verify (
+        {
           onSuccess : function () {
-	     sendResponse ({ok : true});
-	     zendesk_app.init (pref);
-             dbg.log (BG_LOG, 'Zendesk Credentials OK');
-	  },
+	          sendResponse ({ok : true});
+	          zendesk_app.init (pref);
+            dbg.log (BG_LOG, 'Zendesk Credentials OK');
+	        },
           onError   : function () {
-	     sendResponse ({ok : false});
-	     dbg.log (BG_LOG, 'Zendesk Credetials INVALID ');
+	          sendResponse ({ok : false});
+	          dbg.log (BG_LOG, 'Zendesk Credetials INVALID ');
           }
-       }, request.zendesk_url, request.zendesk_usr, request.zendesk_pwd);
+        }, request.zendesk_url, request.zendesk_usr, request.zendesk_pwd);
     }
 
     /** Verify Highrise Account **/
     if ( request.verifyHighrise ) {
-	var highriseResult = {};
-	dbg.log(BG_LOG, 'Verifying Highrise Credentials TOKEN ' + request.highrise_url + '');
-        highrise_app.verifyToken ({
-	    onSuccess : function (data) {
-		dbg.log(BG_LOG, 'HIGHRISE API :: Highrise credentials OK');
-	        sendResponse({ok : true});
-		highrise_app.init(pref);
-	    },
-	    onError   : function () {
-		dbg.log(BG_LOG, 'HIGHRISE API :: Highrise credentials NOT OK');
-		sendResponse({ok : false});
-	    }},
-	    request.highrise_url,
-            request.highrise_token);
+	    var highriseResult = {};
+	    dbg.log(BG_LOG, 'Verifying Highrise Credentials TOKEN ' + request.highrise_url + '');
+      highrise_app.verifyToken (
+        {
+	        onSuccess : function (data) {
+		        dbg.log(BG_LOG, 'HIGHRISE API :: Highrise credentials OK');
+	          sendResponse({ok : true});
+		        highrise_app.init(pref);
+	        },
+	        onError   : function () {
+		        dbg.log(BG_LOG, 'HIGHRISE API :: Highrise credentials NOT OK');
+		        sendResponse({ok : false});
+	        }
+        },
+	      request.highrise_url,
+        request.highrise_token);
     }
 
     /** In case we need to refresh Highrise from the content page **/
     if (request.refreshHighrise && pref && pref.get ('highriseEnabled')) {
-	var f_wait = function() {
-	    dbg.log(BG_LOG, 'HIGHRISE API :: Refreshing Highrise');
-	    highrise_app.init (pref);
-	};
-	/** Wait a couple of seconds for the server side changes to take **/
-	/** affect before we retrieve the latest & greatest.  This code executes **/
-	/** whenever an update is made to the Highrise customer inventory  **/
-	setTimeout(f_wait, 2000);
-	sendResponse ({ok : true});
+	    var f_wait = function() {
+	      dbg.log(BG_LOG, 'HIGHRISE API :: Refreshing Highrise');
+	      highrise_app.init (pref);
+	    };
+	    /** Wait a couple of seconds for the server side changes to take **/
+	    /** affect before we retrieve the latest & greatest.  This code executes **/
+	    /** whenever an update is made to the Highrise customer inventory  **/
+	    setTimeout(f_wait, 2000);
+	    sendResponse ({ok : true});
     }
 });
 
@@ -239,7 +248,7 @@ chrome.extension.onRequest.addListener( function(request, sender, sendResponse) 
 /** Stores a state every time an "active" event is sent, up to 20 items. **/
 /**
 chrome.idle.onStateChanged.addListener     ( function (newstate) {
-    // Rebound BOSH logic 
+    // Rebound BOSH logic
     if (state_inactive.length >= 1) {
 	var d_past      = state_inactive[0].time;
 	var d_now       = new Date();
