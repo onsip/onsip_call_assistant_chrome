784c62709685aaedc2700a30eabe62d0fb7bf5c3
OnSIP Developer <developer@junctionnetworks.com>
2012-07-11 12:19:29 -0400

    modify key usability feature on initial install
    - when the end user clicks the extension's icon near the address bar,
      they are taken to the options page if no credentials have been
      entered. otherwise, with a successful login the extension is toggled
      enabled / disabled.

--- onsip/js/OnSIP-Process.js
+++ onsip/js/OnSIP-Process.js
@@ -1,79 +1,84 @@
 var OnSIP_Process = function() {
 
   var updateBadge = function(text, color, icon, title) {
-	  chrome.browserAction.setBadgeBackgroundColor ({ 'color' : color });
-	  chrome.browserAction.setBadgeText ({ 'text': text });
-	  chrome.browserAction.setTitle ({ title : title });
-	  chrome.browserAction.setIcon  ({ path  : icon  });
+    chrome.browserAction.setBadgeBackgroundColor ({ 'color' : color });
+    chrome.browserAction.setBadgeText ({ 'text': text });
+    chrome.browserAction.setTitle ({ title : title });
+    chrome.browserAction.setIcon  ({ path  : icon  });
   };
 
   var sendPageRequest = function (request) {
-	  var requestOptions = {};
+    var requestOptions = {};
 
-	  switch (request) {
-	  case 'parseDOM' :
-	    requestOptions.parseDOM = true;
-	    break;
-	  case 'clearDOM' :
-	    requestOptions.clearDOM = true;
-	    break;
-	  }
+    switch (request) {
+    case 'parseDOM' :
+      requestOptions.parseDOM = true;
+      break;
+    case 'clearDOM' :
+      requestOptions.clearDOM = true;
+      break;
+    }
 
-	  chrome.windows.getAll(
+    chrome.windows.getAll(
       {populate:true},
       function(windows) {
-	      for (var w in windows) {
-	        for (var t in windows[w].tabs) {
-	          var tabId = windows[w].tabs[t].id;
-		        if (tabId) {
-		          chrome.tabs.sendRequest(tabId, requestOptions);
-		        }
-	        }
-	      }
-	    });
+        for (var w in windows) {
+          for (var t in windows[w].tabs) {
+            var tabId = windows[w].tabs[t].id;
+            if (tabId) {
+              chrome.tabs.sendRequest(tabId, requestOptions);
+            }
+          }
+        }
+    });
   };
 
-  // alias for the OnSIP_Preferences object
   var pref = OnSIP_Preferences;
   return {
-	  init : function () {
-	    var on = pref.get('enabled');
-	    if (on) {
-	      this.enable();
-	    } else {
-	      this.disable();
-	    }
-	  },
+    init : function () {
+      var enabled = pref.get('enabled');
+      if (enabled) {
+        this.enable();
+      } else {
+        this.disable();
+      }
+    },
     enable : function () {
-	    pref.set ('enabled', true);
+      var text	= pref.get('badgeOnText'),
+        color = pref.get('badgeOnColor'),
+        icon  = pref.get('badgeOnIcon'),
+        title = pref.get('badgeOnTitle');
 
-	    var text	= pref.get('badgeOnText');
-	    var color	= pref.get('badgeOnColor');
-	    var icon	= pref.get('badgeOnIcon');
-	    var title	= pref.get('badgeOnTitle');
+      pref.set ('enabled', true);
 
-	    updateBadge (text, color, icon, title);
-	    sendPageRequest('parseDOM');
-	  },
-	  disable : function () {
-	    pref.set('enabled', false);
+      updateBadge (text, color, icon, title);
+      sendPageRequest('parseDOM');
+    },
+    disable : function () {
+      var text	= pref.get('badgeOffText'),
+        color = pref.get('badgeOffColor'),
+        icon  = pref.get('badgeOffIcon'),
+        title = pref.get('badgeOffTitle');
 
-	    var text	= pref.get('badgeOffText');
-	    var color	= pref.get('badgeOffColor');
-	    var icon	= pref.get('badgeOffIcon');
-	    var title	= pref.get('badgeOffTitle');
+      pref.set('enabled', false);
 
-	    updateBadge(text, color, icon, title);
-	    sendPageRequest('clearDOM');
-	  },
-	  toggle : function() {
-	    var ON = pref.get ('enabled');
-	    if ( ON ) {
-	      this.disable();
-	    } else {
-	      this.enable();
-	    }
-	  }
+      updateBadge(text, color, icon, title);
+      sendPageRequest('clearDOM');
+    },
+    toggle : function() {
+      var enabled = pref.get('enabled'),
+        opts = chrome.extension.getURL('options/highrise/index.html');
+
+      if (!pref.get('onsipCredentialsGood')) {
+        chrome.tabs.create({url: opts});
+      } else {
+        if (enabled) {
+          this.disable();
+        } else {
+          this.enable();
+        }
+      }
+    }
   };
 };
 
